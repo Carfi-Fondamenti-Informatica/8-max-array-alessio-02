67292c186b85935c947a7d056422de072cb2545f
alessio-02 <91601426+alessio-02@users.noreply.github.com>
2021-12-07 15:17:16 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,28 +1,9 @@
 #include "lib.h"
-#include <iostream>
 
-float f(float a[], int d1){
-    float temp[1]{0}, temp1[1], temp2[1];
-    if (a[0]>=a[d1-1]){
-        temp1[0]=a[0];
-    }else{
-        temp1[0]=a[d1-1];
-    }
-    for (int i = 0; i < d1-1; i++) {
-        if (temp[0]>=a[i] and a[i+1]){
-            temp2[0]=temp[0];
-        }
-        if (a[i]>a[i+1]){
-            temp[0]=a[i];
-        } else{
-            temp[0]=a[i+1];
+void f1(float a[], int d1){
+    for (int i = 0; i < d1; i++) {
+        if (a[0]<a[i]){
+            a[0]=a[i];
         }
     }
-    if (temp[0]<temp1[0]){
-        temp[0]=temp1[0];
-    }
-    if (temp[0]<temp2[0]) {
-        temp[0] = temp2[0];
-    }
-    return temp[0];
 }
